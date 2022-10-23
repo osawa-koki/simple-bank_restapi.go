e3aaf3b973a889a9779a29977ee1764b3948d7f0
osawa-koki <osawa-koki@osawa.dev>
2022-10-23 09:56:12 +0000

    Acceptヘッダによってコンテントタイプを制御。

--- src/main.go
+++ src/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"net/http"
+  "encoding/json"
+  "encoding/xml"
+  "fmt"
+  "net/http"
 )
 
 // JSON解析用に使用する構造体の定義
@@ -13,54 +13,71 @@ import (
 // XMLを使用する場合には「xml:"★★★"」を書く。
 // ひとつの構造体をJSONとXMLで共有することも可能。
 type Customer struct {
-	Name string `json:"fill_name" xml:"name"`
-	City string `json:"city" xml:"city"`
-	Zipcode string `json:"zip_code" xml:"zipcode"`
+  Name string `json:"fill_name" xml:"name"`
+  City string `json:"city" xml:"city"`
+  Zipcode string `json:"zip_code" xml:"zipcode"`
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+  fmt.Fprint(w, "Hello World")
 }
 
 func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer {
-		{Name: "Osawa Koki", City: "Soka", Zipcode: "340-0021"},
-		{Name: "Sakura Mana", City: "Shibuya", Zipcode: "150-0043"},
-		{Name: "Matz", City: "Tsukuba", Zipcode: "305-8577"},
-	}
+  customers := []Customer {
+    {Name: "Osawa Koki", City: "Soka", Zipcode: "340-0021"},
+    {Name: "Sakura Mana", City: "Shibuya", Zipcode: "150-0043"},
+    {Name: "Matz", City: "Tsukuba", Zipcode: "305-8577"},
+  }
 
-	// レスポンスヘッダにコンテントタイプを指定
-	w.Header().Add("Content-Type", "application/json")
+  // レスポンスヘッダにコンテントタイプを指定
+  w.Header().Add("Content-Type", "application/json")
 
-	// JSON形式にエンコード
-	// IOライターを受け取る。(FPrint関数的な、、、)
-	json.NewEncoder(w).Encode(customers)
+  // JSON形式にエンコード
+  // IOライターを受け取る。(FPrint関数的な、、、)
+  json.NewEncoder(w).Encode(customers)
 }
 
 func getAllCustomers_xml(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer {
-		{Name: "Osawa Koki", City: "Soka", Zipcode: "340-0021"},
-		{Name: "Sakura Mana", City: "Shibuya", Zipcode: "150-0043"},
-		{Name: "Matz", City: "Tsukuba", Zipcode: "305-8577"},
-	}
+  customers := []Customer {
+    {Name: "Osawa Koki", City: "Soka", Zipcode: "340-0021"},
+    {Name: "Sakura Mana", City: "Shibuya", Zipcode: "150-0043"},
+    {Name: "Matz", City: "Tsukuba", Zipcode: "305-8577"},
+  }
 
-	// レスポンスヘッダにコンテントタイプを指定
-	w.Header().Add("Content-Type", "application/xml")
+  // レスポンスヘッダにコンテントタイプを指定
+  w.Header().Add("Content-Type", "application/xml")
 
-	// XML形式にエンコード
-	xml.NewEncoder(w).Encode(customers)
+  // XML形式にエンコード
+  xml.NewEncoder(w).Encode(customers)
+}
+
+func getAllCustomers_flex(w http.ResponseWriter, r *http.Request) {
+  customers := []Customer {
+    {Name: "Osawa Koki", City: "Soka", Zipcode: "340-0021"},
+    {Name: "Sakura Mana", City: "Shibuya", Zipcode: "150-0043"},
+    {Name: "Matz", City: "Tsukuba", Zipcode: "305-8577"},
+  }
+
+  if r.Header.Get("Accept") == "application/xml" {
+    w.Header().Add("Content-Type", "application/xml")
+    xml.NewEncoder(w).Encode(customers)
+  } else {
+    w.Header().Add("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(customers)
+  }
 }
 
 func main() {
 
-	// ハンドラファンクション -> デフォルトmultiplexerに登録。
-	// 第一引数 -> パターン
-	// 第二引数 -> 「レスポンスライター」「リクエスト」を引数として受け取る関数
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-	http.HandleFunc("/customers_xml", getAllCustomers_xml)
+  // ハンドラファンクション -> デフォルトmultiplexerに登録。
+  // 第一引数 -> パターン
+  // 第二引数 -> 「レスポンスライター」「リクエスト」を引数として受け取る関数
+  http.HandleFunc("/greet", greet)
+  http.HandleFunc("/customers", getAllCustomers)
+  http.HandleFunc("/customers_xml", getAllCustomers_xml)
+  http.HandleFunc("/customers_flex", getAllCustomers_flex)
 
-	// 第一引数 -> リッスンするアドレス
-	// 第二引数 -> 使用するmultiplexer | デフォルトで用意されているものを使用するため、nilを設定
-	http.ListenAndServe("localhost:8000", nil)
+  // 第一引数 -> リッスンするアドレス
+  // 第二引数 -> 使用するmultiplexer | デフォルトで用意されているものを使用するため、nilを設定
+  http.ListenAndServe("localhost:8000", nil)
 }
