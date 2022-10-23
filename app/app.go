cd1dbf658415ec1a350b9491b739d1a1348455c6
osawa-koki <osawa-koki@osawa.dev>
2022-10-23 10:21:53 +0000

    multiplexerを自身で作成。

--- app/app.go
+++ app/app.go
@@ -3,15 +3,18 @@ package app
 import "net/http"
 
 func Start() {
-	// ハンドラファンクション -> デフォルトmultiplexerに登録。
+	// 自身でmultiplexerを登録
+	mux := http.NewServeMux()
+
+	// ハンドラファンクション -> デフォルトmultiplexerに登録。 | 自身で作成したmultiplexerを使用
 	// 第一引数 -> パターン
 	// 第二引数 -> 「レスポンスライター」「リクエスト」を引数として受け取る関数
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-	http.HandleFunc("/customers_xml", getAllCustomers_xml)
-	http.HandleFunc("/customers_flex", getAllCustomers_flex)
+	mux.HandleFunc("/greet", greet)
+	mux.HandleFunc("/customers", getAllCustomers)
+	mux.HandleFunc("/customers_xml", getAllCustomers_xml)
+	mux.HandleFunc("/customers_flex", getAllCustomers_flex)
 
 	// 第一引数 -> リッスンするアドレス
 	// 第二引数 -> 使用するmultiplexer | デフォルトで用意されているものを使用するため、nilを設定
-	http.ListenAndServe("localhost:8000", nil)
+	http.ListenAndServe("localhost:8000", mux)
 }
